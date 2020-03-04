d75a80c44b353ee15d61d98d4ee7ef5debf9bfc0
Mitch <kinne174@umn.edu>
2020-03-03 20:04:40 -0600

    changes after debugging, runs fine for cutoff 50

--- utils_real_data.py
+++ utils_real_data.py
@@ -9,7 +9,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-words = brown.words()
+random_words = set(brown.words(categories='news'))
 
 
 class ArcExample(object):
@@ -19,7 +19,7 @@ class ArcExample(object):
         self.sentence_features = [{
             'sentence': s,
             'changed_word_indices': cwi,
-        } for s, st, cwi in zip(sentences, changed_words_indices)]
+        } for s, cwi in zip(sentences, changed_words_indices)]
         self.label = label
         self.sentence_type = sentence_type
 
@@ -52,19 +52,17 @@ def domain_finder(args, question, contexts, answers):
 
 def attention_loader(words):
     # TODO make this based on that one paper with essential learning
-    all_changed_inds = []
-    for _ in range(3):
-        center_index = random.randint(0, len(words)-1)
-        changed_inds = [1 if (center_index - 1) <= i <= (center_index + 2) else 0 for i in range(len(words))]
-        all_changed_inds.append(changed_inds)
-    return all_changed_inds
+    # return 1 change index and then three noisy sentences
+    center_index = random.randint(1, len(words)-2)
+    changed_inds = [1 if (center_index - 1) <= i <= (center_index + 2) else 0 for i in range(len(words))]
+    return changed_inds
 
 
-def noisy_sentences(words, all_changed_indices):
+def noisy_sentences(words, changed_indices):
     # TODO change this to sample something intelligent
     noise_sentences = []
-    for changed_indices in all_changed_indices:
-        noise_words= [w if ci == 0 else random.sample(words, 1) for w, ci in zip(words, changed_indices)]
+    for _ in range(3):
+        noise_words = [w if ci == 0 else random.sample(random_words, 1)[0].lower() for w, ci in zip(words, changed_indices)]
         noise_sentences.append(' '.join(noise_words))
 
     return noise_sentences
@@ -148,7 +146,7 @@ def examples_loader(args):
 
                             changed_words_indices = attention_loader(sentence_words)
 
-                            assert all([len(cwi) == len(sentence_words) for cwi in changed_words_indices]), 'indices length and words length do not match'
+                            assert len(changed_words_indices) == len(sentence_words), 'indices length and words length do not match'
 
                             noise_sentences = noisy_sentences(sentence_words, changed_words_indices)
 
@@ -165,14 +163,14 @@ def examples_loader(args):
                                                            sentence_type=0,
                                                            label=label))
 
-                    if args.cutoff is not None and len(all_examples) >= args.cutoff and subset == 'train':
+                    if args.cutoff is not None and len(all_examples) >= args.cutoff:
                         all_examples = all_examples[:args.cutoff]
                         break
-        else:
-            # TODO
-            # search through corpus for domain words
-            # do this pretty sloppily for right now
-            raise NotImplementedError
+    else:
+        # TODO
+        # search through corpus for domain words
+        # do this pretty sloppily for right now
+        raise NotImplementedError
 
     # make sure there is at least one example
     assert len(all_examples) > 1, 'No examples in the domain!'
