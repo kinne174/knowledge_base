73d6bf854d8e7df321cba53474e56502bb9b2341
Mitch <kinne174@umn.edu>
2020-03-07 01:38:18 -0500

    after debugging models

--- utils_models.py
+++ utils_models.py
@@ -18,9 +18,11 @@ class GraphBlock(nn.Module):
         self.args = args # the input from user
         # self.training = training # a bool if the model is training or not
 
-        self.lstm = nn.LSTM(args.max_length, args.lstm_hidden_dim)
-        self.linear1 = nn.Linear(in_features=args.max_length, out_features=args.max_length)
+        self.lstm = nn.LSTM(100, args.lstm_hidden_dim)
+        self.linear1 = nn.Linear(in_features=300, out_features=100) # TODO make these more functional
         self.linear2 = nn.Linear(args.lstm_hidden_dim, 1)
+
+        self.loss_function = nn.BCEWithLogitsLoss()
 
     def forward(self, knowledge_base, training, **inputs):
         '''
@@ -31,6 +33,7 @@ class GraphBlock(nn.Module):
         '''
 
         input_ids = inputs['input_ids']
+        input_mask = inputs['input_mask']
         labels = inputs['labels']
 
         batch_size = labels.shape[0]
@@ -42,30 +45,31 @@ class GraphBlock(nn.Module):
             # for each input
             for i in range(4):
                 # subset graph based on current input, grab nodes of inputs and first neighbor nodes and all corresponding edges, create a copy so can change edges/ nodes
-                current_input_ids = input_ids[b, i, :]
+                current_input_ids = input_ids[b, i, :torch.sum(input_mask[b, i, :])]
                 subset_knowledge_base, ids_mapping = self.subset_graph(knowledge_base, current_input_ids)
 
                 # if training cast edges to {0, 1} in a bernoulli fashion based on word2vec function, else return as is
                 subset_knowledge_base = self.cast_edges(subset_knowledge_base, training)
 
                 # aggregate nodes in a mean/ summing way, can try to do this with dgl
-                subset_knowledge_base.update_all(df.u_mul_e('embeddings', 'value', 'embeddings_'), df.mean('embeddings_', 'embeddings_mean'))
+                subset_knowledge_base.update_all(message_func=df.u_mul_e('embedding', 'value', 'embedding_'),
+                                                 reduce_func=df.mean('embedding_', out='embedding_mean'))
 
                 # MLP on aggregated nodes, not clear if output needs to be the same dimension or not
-                h = subset_knowledge_base.ndata.pop('embeddings_mean')
+                h = subset_knowledge_base.ndata.pop('embedding_mean')
                 transformed_nodes = self.linear1(h)
 
                 # LSTM on transformed nodes in order of the input
-                lstm_transformed_nodes = torch.cat([transformed_nodes[cui, :] for cui in current_input_ids if not cui == 0], dim=0).unsqueeze(1)
-                lstm_out, _ = self.lstm(lstm_transformed_nodes)
+                lstm_transformed_nodes = torch.cat([transformed_nodes[ids_mapping[int(cui)], :] for cui in current_input_ids]).reshape((current_input_ids.shape[0], 1, -1))
+                lstm_out, (last_hidden, last_cell) = self.lstm(lstm_transformed_nodes)
 
                 # sentence embedding through an MLP to get a scalar score of accuracy
-                answer_score = self.linear2(lstm_out.squeeze())
+                answer_score = self.linear2(last_hidden.squeeze())
 
                 all_answer_scores[b, i] = answer_score
 
         # soft max over logits, may need to transform again if output from MLP is not "correct"
-        softmaxed_scores = F.log_softmax(all_answer_scores, dim=1)
+        softmaxed_scores = F.softmax(all_answer_scores, dim=1)
 
         # calculate error using Binary Cross Entropy loss
         error = self.loss_function(softmaxed_scores, labels)
@@ -84,8 +88,8 @@ class GraphBlock(nn.Module):
 
         unique_ids = torch.unique(input_ids).tolist()
 
-        adj_matrix = tril(G.adjacency_matrix_scipy(), format('csr'))
-        assert isinstance(adj_matrix, csr_matrix)
+        adj_matrix = G.adjacency_matrix_scipy()
+        assert adj_matrix.format == 'csr'
 
         indptr = adj_matrix.indptr.tolist()
         indices = adj_matrix.indices.tolist()
@@ -112,7 +116,7 @@ class GraphBlock(nn.Module):
         new_G = dgl.DGLGraph()
 
         new_G.add_nodes(len(all_nodes))
-        new_G.ndata['embeddings'] = G.ndata['embeddings'][all_nodes]
+        new_G.ndata['embedding'] = G.ndata['embedding'][all_nodes]
 
         new_G.add_edges(new_src, new_dest)
         new_G.add_edges(new_dest, new_src)
@@ -123,9 +127,9 @@ class GraphBlock(nn.Module):
         new_src_dest = new_src + new_dest
         new_dest_src = new_dest + new_src
 
-        new_G.edges[new_src_dest, new_dest_src].data['value'] = G.edges[src_dest, dest_src].data['value']
+        new_G.edata['value'] = G.edges[src_dest, dest_src].data['value']
 
-        ids_mapping = {ui: all_nodes.index(ui) for ui in torch.unique(all_nodes)}
+        ids_mapping = {ui: all_nodes.index(ui) for ui in all_nodes}
 
         return new_G, ids_mapping
 
@@ -135,13 +139,13 @@ class GraphBlock(nn.Module):
 
         assert isinstance(G, dgl.DGLGraph)
 
-        edge_values = G.edata['value'].squeeze().tolist()
+        edge_values = G.edata['value'].tolist()
 
         def f(edge_val, parameter):
             # TODO write a better casting function
-            return int(round(edge_val))
+            return 1. # make sure returning a float
 
-        new_edge_values = torch.tensor([f(e, self.args.edge_paramter) for e in edge_values]).reshape((-1, 1))
+        new_edge_values = torch.tensor([f(e, self.args.edge_parameter) for e in edge_values]).reshape((-1,))
 
         G.edata['value'] = new_edge_values
 
