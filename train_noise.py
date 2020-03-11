7baf629eb69828eb66cea55378471e067fd2f017
Mitch <kinne174@umn.edu>
2020-03-11 16:18:41 -0500

    commenting and adding translation dict to saved objects

--- train_noise.py
+++ train_noise.py
@@ -15,53 +15,66 @@ import getpass
 #logging
 logger = logging.getLogger(__name__)
 
+# whether able to use gpu or not
 def get_device():
     if torch.cuda.is_available():
         return torch.device('cuda')
     else:
         return torch.device('cpu')
 
+# don't want to have stop words in answers be labeled important so remove them when in answers
 stop_words = set(stopwords.words('english'))
-
 
 def set_seed(args):
     torch.manual_seed(args.seed)
     # if args.n_gpu > 0:
     #     torch.cuda.manual_seed_all(args.seed)
 
+# function to run through data provided by "Learning What is Essential in Questions" Khashabi et al. and produce
+# a tokenized list of all question answer pairings, a label of each word 0 <= ell <= 1, input masks to say where
+# non-padding tokens are, and a word to id dict to map words to their tokens
 def get_words(cutoff):
     data_filename = '../ARC/essential_data/turkerSalientTermsWithOmnibus-v3.tsv'
     words_list = []
     labels_list = []
 
+    # record number of entries lost to no answers
     num_lost = 0
 
     with open(data_filename, 'r') as tsv_file:
         tsv_reader = csv.reader(tsv_file)
 
         for line in tsv_reader:
+            # wasn't sure how to split by tabs so did it manually
             line = ''.join(line)
             line = line.split('\t')
 
+            # separate text into the three categories
             question_text = line[0]
             num_annotators = int(line[1])
-            annotations = line[2]
+            annotations = line[2] # this is the raw number of annotators that believed it was an important word
 
+            # from looking at data this is how all answers are indicated
             answer_letters = ['(A)', '(B)', '(C)', '(D)', '(1)', '(2)', '(3)', '(4)']
 
+            # if no answers provided note and continue without adding to returned data
             if not any([answer_letter in question_text for answer_letter in answer_letters]):
                 logger.info('One of {} is not in the question text: {}. Skipping.'.format(', '.join(answer_letters), question_text))
                 num_lost += 1
                 continue
 
+            # get the actual answer indicators used
             answer_letters = [a_l for a_l in answer_letters if a_l in question_text]
 
+            # where each answer begins/ends in question text, should have len(answer_letters) + 1 elements
             answer_inds = [question_text.index(answer_letter) for answer_letter in answer_letters] + [len(question_text)]
 
+            # beginning and ending indice for each answer, should have len(answer_letters) elements
             answer_ind_tuples = [(answer_inds[i], answer_inds[j]) for i, j in zip(range(len(answer_inds)-1), range(1, len(answer_inds)))]
 
             answers = []
             answer_scores = []
+            # record and store each answer, assign each answer word a 1 indicating all annotators would have thought it to be important
             for t in answer_ind_tuples:
                 answer_words = question_text[t[0]:t[1]].rstrip().split(' ')
                 answer_words = [aw.lower() for aw in answer_words[1:] if aw not in stop_words]
@@ -69,14 +82,18 @@ def get_words(cutoff):
                 answers.append(answer_words)
                 answer_scores.append([1.]*len(answer_words))
 
+            # get words and their scores
             annotations = annotations.split('|')
             words_and_scores = [(annotation[:-1].lower(), int(annotation[-1])) for annotation in annotations]
             assert all([score <= num_annotators for _, score in words_and_scores])
 
+            # change from raw score to percentage of annotators
             words_and_percents = [(w, s/num_annotators) for w, s in words_and_scores]
 
+            # split apart
             q_words, percents = map(list, zip(*words_and_percents))
 
+            # add answer words and answer scores to end of question words and percents
             for answer, answer_score in zip(answers, answer_scores):
 
                 words = ['[BOS]'] + q_words + answer + ['[EOS]']
@@ -94,44 +111,65 @@ def get_words(cutoff):
     padding_tokens = ['[EOS]', '[BOS]']
     token_list = []
     word_to_idx = {'[PAD]': 0, '[BOS]': 1, '[EOS]': 2}
+    # create a dictionary to translate from words to tokens to be used in an embedding matrix
+    # also edits labels, and words to fix typos in sentences, specifically two words sticking together, i.e. oceanbreeze -> ocean breeze
     for ind1, (sent, labels) in enumerate(zip(words_list, labels_list)):
         new_sent = []
         new_labels = []
         for ind2, (word, label) in enumerate(zip(sent, labels)):
+            # remove non numbers and letters, unless it's a word used in padding
             word = ''.join([c for c in word if c.isalnum()]) if word not in padding_tokens else word
 
+            # try to find and fix minor typos
             if not nlp.vocab.has_vector(word) and word not in padding_tokens:
+                # create a list of proposed pairs of words by adding a space in between each letter
                 spaced_tokens = [(word[:i], word[i:]) for i in range(1, len(word))]
+                # see if both spaced words have a word vector
                 temp = [all((nlp.vocab.has_vector(st[0]), nlp.vocab.has_vector(st[1]))) for st in spaced_tokens]
+
+                # if any do then update labels and words to replace typo word with two new words that share the label of the typo word
                 if any(temp):
                     if sum(temp) > 1:
                         logger.info('More than one acceptable pairing of words: {}'.format(' '.join(['({}, {})'.format(st[0], st[1]) for st, t in zip(spaced_tokens, temp) if t])))
+
+                    # only look at first success, may not be true
                     best_words = spaced_tokens[temp.index(True)]
+
+                    # add each word to translation dict and update new sentence and labels
                     for best_word in best_words:
                         if best_word not in word_to_idx:
                             word_to_idx[best_word] = len(word_to_idx)
 
                         new_sent.append(word_to_idx[best_word])
                     new_labels.extend([label]*2)
+
+                    # continue to make sure the typo word is not added to the translation dict
                     continue
 
+            # if the word had an embedding vector add it to the translation dict if not in already
             if word not in word_to_idx:
                 word_to_idx[word] = len(word_to_idx)
 
+            # update current sentence with token and label
             new_sent.append(word_to_idx[word])
             new_labels.append(label)
 
         assert len(new_sent) == len(new_labels)
 
+        # add new tokenized sentence and edit labels in case a typo word was replaced
         token_list.append(new_sent)
         labels_list[ind1] = new_labels
 
+    # find max length of tokenized sentence for padding purposes
+    # need to pad so input is the same size for MLP in classification, not neccessary for LSTM though
     max_length = max([len(t) for t in token_list])
 
     all_input_masks = []
     all_tokens_list = []
     all_labels_list = []
 
+    # pad token and labels up to max length
+    # also create input masks to identify where padding is, EOS and BOS are not considered padding here, not sure if that's correct
     for tokens, labels in zip(token_list, labels_list):
         assert len(tokens) == len(labels), 'The length of tokens is {} and the length of labels is {}'.format(len(tokens), len(labels))
         padding_length = max_length - len(tokens)
@@ -146,11 +184,14 @@ def get_words(cutoff):
     return all_tokens_list, all_input_masks, all_labels_list, word_to_idx
 
 
+# create an embedding matrix from GloVe vectors
 def create_embedding_matrix(word_to_idx):
+    # known a priori that each vector has dimension 300
     embedding_matrix = torch.empty((len(word_to_idx), 300))
 
     assert isinstance(word_to_idx, dict)
     for word, id in word_to_idx.items():
+        # create an embedding vector in row of token if an embedding exists, otherwise put random noise there
         if nlp.vocab.has_vector(word):
             embedding_matrix[id, :] = torch.tensor(nlp.vocab.get_vector(word))
 
@@ -172,6 +213,7 @@ class LSTM2MLP(nn.Module):
         self.lstm = nn.LSTM(self.embedding_dim, self.hidden_dim, num_layers=2, batch_first=True, bidirectional=True, dropout=0.1)
         self.linear = nn.Sequential(nn.Linear(self.hidden_dim, 1),
                                     nn.Sigmoid())
+        # using standard MSE loss
         self.loss = nn.MSELoss(reduction='none')
 
         self.device = args.device
@@ -181,26 +223,26 @@ class LSTM2MLP(nn.Module):
 
         batch_size = input_ids.shape[0]
 
+        # when doing analysis add a BOS and EOS token and labels of zeros on each end
         if add_special_characters:
             input_ids = torch.cat((torch.ones((batch_size, 1), dtype=torch.long), input_ids, 2*torch.ones((batch_size, 1), dtype=torch.long)), dim=1)
             labels = torch.cat((torch.zeros((batch_size, 1), dtype=torch.float), labels, torch.zeros((batch_size, 1), dtype=torch.float)), dim=1)
 
         max_length = input_ids.shape[1]
 
+        # when doing analysis initialize input mask to indicate no padding, expecting everything to come through one at a time
         if input_masks is None:
             input_masks = torch.ones((batch_size, max_length), dtype=torch.long)
             if add_special_characters:
                 input_masks = torch.cat((torch.zeros((batch_size, 1), dtype=torch.long), input_masks, torch.zeros((batch_size, 1), dtype=torch.long)), dim=1)
 
-        # now these are max_length x batch_size
-        # labels = labels.t()
-        # input_masks = input_masks.t()
-
+        # from tokens create embedded sentences from embedding matrix
         inputs = torch.empty((batch_size, max_length, self.embedding_dim)).to(self.device)
         for s_ind, sentence in enumerate(input_ids):
             for m_ind in range(max_length):
                 inputs[s_ind, m_ind, :] = self.embedding_matrix[sentence[m_ind]]
 
+        # get output of each input
         lstm_out, (last_hidden, last_cell) = self.lstm(inputs)
         lstm_out = torch.mean(lstm_out.view(batch_size, max_length, 2, self.hidden_dim), dim=2)
 
@@ -209,20 +251,27 @@ class LSTM2MLP(nn.Module):
 
         # expecting out_scores to be batch_size*max_length x 1
         out_errors = self.loss(out_scores, labels.reshape((-1, 1)))
+
+        # get rid of errors from the padding
         out_errors = out_errors * input_masks.reshape((-1, 1))
+
+        # only count erros from the non-padded tokens
         sum_errors = torch.sum(out_errors.view((batch_size, max_length)), dim=1)
         sum_masks = torch.sum(input_masks, dim=1)
         out_errors = torch.mean(sum_errors/sum_masks)
 
+        # reshape the scores to reflect the input dimension
         out_scores = out_scores.reshape((batch_size, max_length))
 
+        # get rid of scores of BOS and EOS tokens
         if add_special_characters:
             out_scores = out_scores[:, 1:-1]
 
         return out_scores, out_errors
 
 
-def save_model(model, embedding_matrix, hidden_dim):
+# save the model parameters, embedding matrix, and translation dict
+def save_model(model, embedding_matrix, hidden_dim, word_to_idx):
     model_save_file = 'saved/train_noise/essential_terms_hidden{}.pt'.format(hidden_dim)
 
     with open(model_save_file, 'wb') as mf:
@@ -233,7 +282,13 @@ def save_model(model, embedding_matrix, hidden_dim):
     with open(embedding_matrix_save_file, 'wb') as ef:
         torch.save(embedding_matrix, ef)
 
+    word_to_idx_save_file = 'saved/train_noise/word_to_idx.py'
 
+    with open(word_to_idx_save_file, 'wb') as wf:
+        torch.save(word_to_idx, wf)
+
+
+# to be used in analysis, load model and translation dict
 def load_model(args):
     filenames = glob.glob('saved/train_noise/*hidden*.pt')
 
@@ -250,9 +305,15 @@ def load_model(args):
     model.load_state_dict(torch.load(model_filename[0]))
     model.eval()
 
-    return model
+    word_to_idx_save_file = 'saved/train_noise/word_to_idx.py'
+
+    with open(word_to_idx_save_file, 'rb') as wf:
+        word_to_idx = torch.load(word_to_idx_save_file)
+
+    return model, word_to_idx
 
 
+# train and save model
 def train(args):
 
     all_token_list, all_input_masks, all_labels_list, word_to_idx = get_words(args.cutoff)
@@ -262,6 +323,7 @@ def train(args):
     # throw embedding matrix to device
     embedding_matrix = embedding_matrix.to(args.device)
 
+    # initialize model
     model = LSTM2MLP(embedding_matrix=embedding_matrix, args=args)
 
     # throw model to device
@@ -270,12 +332,14 @@ def train(args):
     # intiailize optimizer
     optimizer = optim.Adam(params=model.parameters())
 
+    # create TorchTensorDatset
     tokens_tensor = torch.tensor(all_token_list, dtype=torch.long)
     input_masks_tensor = torch.tensor(all_input_masks, dtype=torch.long)
     labels_tensor = torch.tensor(all_labels_list, dtype=torch.float)
 
     dataset = TensorDataset(tokens_tensor, input_masks_tensor, labels_tensor)
 
+    # create sampler to process batches
     train_sampler = RandomSampler(dataset, replacement=False)
     train_dataloader = DataLoader(dataset, sampler=train_sampler, batch_size=args.batch_size)
 
@@ -289,6 +353,7 @@ def train(args):
         epoch_iterator = tqdm(train_dataloader, desc="Iteration, batch size {}".format(args.batch_size))
         for iteration, batch in enumerate(epoch_iterator):
 
+            # clear gradients in model
             model.zero_grad()
 
             # get batch
@@ -315,7 +380,7 @@ def train(args):
 
             logger.info('The error is {}'.format(error))
 
-    save_model(model, embedding_matrix, args.hidden_dim)
+    save_model(model, embedding_matrix, args.hidden_dim, word_to_idx)
 
 
 if __name__ == '__main__':
